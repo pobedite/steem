ea878bcb1765b22928a18bde8bbaa0fd85f68b93
Michael Vandeberg <vandeberg@steemit.com>
2017-01-31 16:17:31 -0500

    Fix build #773

--- libraries/chain/database.cpp
+++ libraries/chain/database.cpp
@@ -2034,7 +2034,7 @@ void database::cashout_comment_helper( const comment_object& comment )
             {
                auto benefactor_tokens = ( author_tokens * b.second ) / STEEMIT_100_PERCENT;
                auto vest_created = create_vesting( get_account( b.first ), benefactor_tokens );
-               push_virtual_operation( comment_benefactor_reward_operation( b.first, comment.author, comment.permlink, vest_created ) );
+               push_virtual_operation( comment_benefactor_reward_operation( b.first, comment.author, to_string( comment.permlink ), vest_created ) );
                author_tokens -= benefactor_tokens;
             }
 
